f23421075321d40f99131470545f91c0bfef1ad6
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2023-12-16 00:38:42 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String[] args) {
+              RWOnly r=new RWOnly();
+              r.setName("Prince");
+              System.out.println("Name: "+ r.getName());
+    }
 }
